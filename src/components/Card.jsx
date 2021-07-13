da12bcb0e1e659dc3b920fc535183fe7a496a7a2
David <davidtucu@hotmail.com>
2021-07-13 12:48:39 -0300

    first commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Avatar from "./Avatar";
+import Detail from "./Detail";
+
+function Card(props) {
+    return(
+    
+    <div className="card">
+      <div className="top">
+        <h2 className="name">{props.name}</h2>
+        <Avatar imgURL={props.imgURL} />
+      </div>
+      <div className="bottom">
+      <Detail text ={props.phone} />
+      <Detail text ={props.email} />
+      </div>
+    </div>
+ )
+  }
+
+  export default Card;
---
+++ src/components/Detail.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+function Detail(props){
+    return(
+        <p className="info">{props.text}</p>
+    );
+}
+
+export default Detail;
